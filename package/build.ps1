dd3c7b52aaad16f3a26898db6c7e3c06e106b127
joepvtl <joepvtl@gmail.com>
2020-10-01 12:06:00 +0200

    Add build script for Nuget and Umbraco. Cleanup solution and change folder names

---
+++ package/build.ps1
@@ -0,0 +1,19 @@
+﻿$Version = Read-Host -Prompt 'Version'
+$Pack = Read-Host -Prompt 'Pack (umb, nuget, both)'
+
+if($Pack -eq 'nuget' -or $Pack -eq 'both'){
+
+    nuget pack package.nuspec -Version $Version
+
+    Write-Host "Succesfully created Nuget package with version $($Version)"
+}
+
+if($Pack -eq 'umb' -or $Pack -eq 'both'){
+
+    umbpack pack package.xml -v $Version
+
+    Write-Host "Succesfully created Umbraco package with version $($Version)"
+}
+
+
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
