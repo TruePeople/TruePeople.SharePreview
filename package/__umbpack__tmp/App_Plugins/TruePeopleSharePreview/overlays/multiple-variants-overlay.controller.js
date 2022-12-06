e8f9b74c3d15d2b761efe68a2c10774b539b66e8
Joep van 't Land <joep@truepeople.nl>
2022-12-06 15:24:48 +0100

    Delete umbpack files

--- package/__umbpack__tmp/App_Plugins/TruePeopleSharePreview/overlays/multiple-variants-overlay.controller.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module("umbraco").controller("TPSharePreview.MultiVariants.Overlay.controller",
-    function ($scope, sharePreviewCopyService) {
-
-        $scope.copyShareLink = function (link) {
-            sharePreviewCopyService.copyShareLink(link);
-        }
-    });
