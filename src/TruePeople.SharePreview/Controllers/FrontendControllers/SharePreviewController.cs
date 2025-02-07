f0a774a3d91c0b8a89dd620ecbc4a7499c8ba905
tiborvanwijk-truepeople <tiborvanwijk@truepeople.nl>
2025-02-07 14:46:36 +0100

    Revert "Revert "Feature/umbraco 13 support""

--- src/TruePeople.SharePreview/Controllers/FrontendControllers/SharePreviewController.cs
+++ src/TruePeople.SharePreview/Controllers/FrontendControllers/SharePreviewController.cs
@@ -18,6 +18,10 @@ using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Extensions;
 using Umbraco.Cms.Core.Routing;
+using Microsoft.AspNetCore.Http.Extensions;
+using HttpContext = Microsoft.AspNetCore.Http.HttpContext;
+using System.Collections.Generic;
+using Umbraco.Cms.Core.PublishedCache;
 
 namespace TruePeople.SharePreview.Controllers.FrontendControllers
 {
@@ -31,6 +35,8 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
         private readonly IUmbracoContextAccessor _umbracoContextAccessor;
         private readonly ILocalizationService _localizationService;
         private readonly ILogger<SharePreviewController> _logger;
+        private readonly UriUtility _uriUtility;
+        private readonly IPublishedRouter _publishedRouter;
 
         public SharePreviewController(
             ISharePreviewSettingsService settingsService,
@@ -39,7 +45,9 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
             IUmbracoContextAccessor umbracoContextAccessor,
             ILocalizationService localizationService,
             ILogger<SharePreviewController> logger,
-            ICompositeViewEngine viewEngine) : base(logger, viewEngine)
+            ICompositeViewEngine viewEngine,
+            IPublishedRouter publishedRouter,
+            UriUtility uriUtility) : base(logger, viewEngine)
         {
             _sharePreviewSettings = settingsService;
             _contentService = contentService;
@@ -47,11 +55,19 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
             _umbracoContextAccessor = umbracoContextAccessor;
             _localizationService = localizationService;
             _logger = logger;
+            _publishedRouter = publishedRouter;
+            _uriUtility = uriUtility;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
+            // We set the variationContext again because on some umbraco versions it gets overridden.
+            var settings = _sharePreviewSettings.GetSettings();
+            var decrypted = TPEncryptHelper.DecryptString(RouteData.Values["pageId"].ToString(), settings.PrivateKey);
+            var sharePreviewContext = JsonConvert.DeserializeObject<SharePreviewContext>(decrypted);
+            _variationContextAccessor.VariationContext = new VariationContext(sharePreviewContext.Culture);
+
             var viewResult = View(CurrentPage.GetTemplateAlias(), CurrentPage);
             return viewResult;
         }
@@ -63,7 +79,7 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
                 return null;
 
             var settings = _sharePreviewSettings.GetSettings();
-
+            
             //Decode first layer of the base64 string;
             //Then decode second layer and decrypt it with private key.
             try
@@ -92,6 +108,7 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
 
                         var page = umbracoContext.Content.GetById(true, sharePreviewContext.NodeId);
                         PrepareRequest(actionExecutingContext);
+                        umbracoContext.PublishedRequest ??= CreatePublishedRequest(actionExecutingContext.HttpContext, page, sharePreviewContext.Culture);                            
                         return page;
                     }
                 }
@@ -105,6 +122,7 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
                     var defaultCulture = _localizationService.GetDefaultLanguageIsoCode();
                     Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new CultureInfo(defaultCulture);
                     PrepareRequest(actionExecutingContext);
+                    umbracoContext.PublishedRequest ??= CreatePublishedRequest(actionExecutingContext.HttpContext, page, sharePreviewContext.Culture);
                     return page;
                 }
 
@@ -123,6 +141,32 @@ namespace TruePeople.SharePreview.Controllers.FrontendControllers
 
             //When it gets here something went wrong...
             return null;
+        }
+        // We create our own published request because umbraco does not make a published request in a custom mvc route's
+        private IPublishedRequest CreatePublishedRequest(HttpContext httpContext, IPublishedContent publishedContent, string culture)
+        {
+            var originalRequestUrl = new Uri(httpContext.Request.GetEncodedUrl());
+            Uri cleanedUri = _uriUtility.UriToUmbraco(originalRequestUrl);
+            var domainAndUri = GetDomainAndUriForRoute(cleanedUri, culture);
+            IPublishedRequestBuilder requestBuilder = _publishedRouter.CreateRequestAsync(cleanedUri).GetAwaiter().GetResult();
+
+            requestBuilder.SetPublishedContent(publishedContent);
+            requestBuilder.SetDomain(domainAndUri);
+            requestBuilder.SetCulture(culture);
+            
+            return requestBuilder.Build();
+        }
+        private DomainAndUri? GetDomainAndUriForRoute(Uri contentUrl, string culture)
+        {
+            IDomainCache? domainCache = _umbracoContextAccessor.GetRequiredUmbracoContext().PublishedSnapshot.Domains;
+            if (domainCache == null)
+            {
+                throw new InvalidOperationException("Could not obtain the domain cache in the current context");
+            }
+
+            IEnumerable<DomainAndUri> domains = domainCache.GetAll(false).Where(d => !d.IsWildcard).Select(d => new DomainAndUri(d, contentUrl));
+
+            return DomainUtilities.SelectDomain(domains, contentUrl, culture, domainCache.DefaultCulture);
         }
 
         private void PrepareRequest(ActionExecutingContext req)
